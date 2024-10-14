4f0d14ebaf41045e71072a329b7fca0391e1168f
Susan Navarro <navarrolalupu7@gmail.com>
2024-10-13 20:23:00 -0500

    Implementacion de cierre sesion

--- src/main/java/pe/edu/cibertec/patitas_frontend_wc/controller/LoginControllerAsync.java
+++ src/main/java/pe/edu/cibertec/patitas_frontend_wc/controller/LoginControllerAsync.java
@@ -1,8 +1,10 @@
 package pe.edu.cibertec.patitas_frontend_wc.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
+import pe.edu.cibertec.patitas_frontend_wc.client.AutenticacionClient;
 import pe.edu.cibertec.patitas_frontend_wc.dto.LoginRequestDTO;
 import pe.edu.cibertec.patitas_frontend_wc.dto.LoginResponseDTO;
 import pe.edu.cibertec.patitas_frontend_wc.dto.LogoutResponseDTO;
@@ -15,6 +17,9 @@ public class LoginControllerAsync {
 
     @Autowired
     WebClient webClientAutenticacion;
+
+    @Autowired
+    AutenticacionClient autenticacionClient;
 
     @PostMapping("/autenticar-async")
     public Mono<LoginResponseDTO> autenticar(@RequestBody LoginRequestDTO loginRequestDTO) {
@@ -55,28 +60,25 @@ public class LoginControllerAsync {
 
     @PostMapping("/logout")
     public Mono<LogoutResponseDTO> logoutUser (@RequestBody String nombreUsuario) {
-
+        System.out.println("Consumiendo con Spring Cloud!");
         if (nombreUsuario.isBlank()){
             return Mono.just(new LogoutResponseDTO(false, null,"Error: Ocurrio un error al hacer la petecion de logout"));
         }
-
         try{
-            return webClientAutenticacion.post()
-                    .uri("/logout")
-                    .body(Mono.just(nombreUsuario), String.class)
-                    .retrieve()
-                    .bodyToMono(LogoutResponseDTO.class)
-                    .flatMap(response ->{
-                        if(response.resultado()){
-                            return Mono.just(new LogoutResponseDTO(true, null, "Se cerro sesion correctamente"));
-                        } else {
-                            return Mono.just(new LogoutResponseDTO(false, null,"Error: Logout fallida"));
-                        }
-                    });
+            ResponseEntity<LogoutResponseDTO> responseLogout = autenticacionClient.logout(nombreUsuario);
+            if (responseLogout.getStatusCode().is2xxSuccessful()){
+                LogoutResponseDTO logoutResponseDTO = responseLogout.getBody();
+                if(logoutResponseDTO.resultado()) {
+                    return Mono.just(logoutResponseDTO);
+                }else{
+                    return Mono.just(new LogoutResponseDTO(false,null,"Error: Logout Fallo"));
+                }
+            }else{
+                return  Mono.just(new LogoutResponseDTO(false, null, "Error: Logout Fallo"));
+            }
         }catch(Exception e) {
             System.out.println(e.getMessage());
-            return Mono.just(new LogoutResponseDTO(false,null, "Error: Ocurrió un problema en la logout"));
+            throw new  RuntimeException(e.getMessage());
         }
     }
-
 }
